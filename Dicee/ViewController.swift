064f4468bcea116278e93e4da1d01dd862a15fda
Tun Lin Thu <tunlinthu94@gmail.com>
2019-08-26 20:46:02 +0900

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Tun Lin Thu on 2019/08/26.
+//  Copyright © 2019 Tun Lin Thu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
