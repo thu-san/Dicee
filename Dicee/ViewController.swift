8a643cb0661b3572f6e9d1811cee135a68a6f61f
Tun Lin Thu <tunlinthu94@gmail.com>
2019-08-27 14:14:03 +0900

    Complete Dicee lecture

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateDiceImages()
     }
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        diceImageView1.image = UIImage(named: "dice\(Int.random(in: 1...6))")
+        diceImageView2.image = UIImage(named: "dice\(Int.random(in: 1...6))")
+    }
 
 }
 
